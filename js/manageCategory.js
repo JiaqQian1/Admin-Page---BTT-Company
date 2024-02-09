188a6ff7c689ddd2ad971a3ac8da0592d02d7f8c
Pxuan04 <1221201836@mmu.edu.my>
2024-02-09 11:53:34 +0800

    done

--- js/manageCategory.js
+++ js/manageCategory.js
@@ -71,7 +71,7 @@ function editCategory(categoryID) {
         // Create a form dynamically and submit it
         const form = document.createElement('form');
         form.method = 'post';
-        form.action = ''; // Leave it empty to submit to the same page
+        form.action = 'manageCategory.php'; // Leave it empty to submit to the same page
 
         const hiddenField1 = document.createElement('input');
         hiddenField1.type = 'hidden';
